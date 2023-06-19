0613828e63be58c41ce90c696fb3906413a13403
fabriciovo_dws <fabricio.varisco@outlook.com>
2023-06-19 13:35:22 -0300

    Initial Commit

---
+++ cpp-image-filters/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp-image-filters
+//
+//  Created by fabricio varisco oliveira on 19/06/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
